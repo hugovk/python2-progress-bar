e715da7533d00887adbbad143c94e777d19c79f2
Hugo van Kemenade <hugovk@users.noreply.github.com>
2020-01-01 06:34:15 +0200

    Others went for the start of today

--- main.js
+++ main.js
@@ -8,8 +8,8 @@ window.onload = function onLoad() {
     var now = new Date();
     // 2010-07-03
     var start = new Date(Date.UTC(2010, 6, 3));
-    // 2020-01-01 23:59:59
-    var end = new Date(Date.UTC(2020, 0, 1, 23, 59, 59));
+    // 2020-01-01 00:00:00
+    var end = new Date(Date.UTC(2020, 0, 1, 0, 0, 0));
     console.log(end)
     var done = (now-start) / (end-start);
     document.getElementById("percent").innerHTML = done*100 + "%";
